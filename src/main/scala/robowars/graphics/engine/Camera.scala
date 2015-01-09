9ae053a13b3400029ce2c89bdd2069f1fe514160
Clemens Winter <clemenswinter1+git@gmail.com>
2015-01-09 16:27:39 +0100

    add Camera position

--- src/main/scala/robowars/graphics/engine/Camera.scala
+++ src/main/scala/robowars/graphics/engine/Camera.scala
@@ -4,38 +4,62 @@ import robowars.graphics.matrices._
 
 
 class Camera {
-  private[this] var screenWidth: Int = 0
-  private[this] var screenHeight: Int = 0
+  private[this] var _screenWidth: Int = 0
+  private[this] var _screenHeight: Int = 0
   private[this] var _projection: Matrix4x4 = IdentityMatrix4x4
-  
+  private[this] var _x: Float = 0
+  private[this] var _y: Float = 0
 
 
   def projection = _projection
 
   private def recomputeProjection() = {
-    _projection = new OrthographicProjectionMatrix4x4(screenWidth, screenHeight)
+    _projection =
+      new OrthographicProjectionMatrix4x4(_screenWidth, _screenHeight) *
+        new TranslationXYMatrix4x4(-x, -y)
   }
 
 
-  def screenDims = (screenWidth, screenHeight)
-
+  def screenDims = (_screenWidth, _screenHeight)
   def screenDims_=(dims: (Int, Int)) = {
-    screenWidth = dims._1
-    screenHeight = dims._2
+    _screenWidth = dims._1
+    _screenHeight = dims._2
 
     recomputeProjection()
   }
 
+  def screenWidth = _screenWidth
   def screenWidth_=(width: Int) = {
-    screenWidth = width
+    _screenWidth = width
 
     recomputeProjection()
   }
 
+  def screenHeight = _screenHeight
   def screenHeight_=(height: Int) = {
-    screenHeight = height
+    _screenHeight = height
 
     recomputeProjection()
+  }
+
+  def position = (_x, _y)
+  def position_=(xy: (Int, Int)) = {
+    _x = xy._1
+    _y = xy._2
+
+    recomputeProjection()
+  }
+
+  def x = _x
+  def x_=(x: Float) = {
+    _x = x
+
+    recomputeProjection()
+  }
+
+  def y = _y
+  def y_=(y: Float) = {
+    _y = y
   }
 
 }
