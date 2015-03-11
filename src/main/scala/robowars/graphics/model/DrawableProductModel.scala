4e8414036f2bfed87401ce79633970d4743b993d
Clemens Winter <clemenswinter1+git@gmail.com>
2015-03-11 21:08:28 +0100

    add ProductModel and DrawableProductModel

---
+++ src/main/scala/robowars/graphics/model/DrawableProductModel.scala
@@ -0,0 +1,33 @@
+package robowars.graphics.model
+
+import robowars.graphics.materials.Material
+import robowars.graphics.matrices.Matrix4x4
+
+
+class DrawableProductModel(model1: DrawableModel, model2: DrawableModel) extends DrawableModel {
+  override def draw(): Unit = {
+    model1.draw()
+    model2.draw()
+  }
+
+  override def setModelview(modelview: Matrix4x4): Unit = {
+    model1.setModelview(modelview)
+    model2.setModelview(modelview)
+  }
+
+  override def project(material: Material[_, _]): DrawableModel = {
+    (model1.project(material), model2.project(material)) match {
+      case (EmptyModel, EmptyModel) => EmptyModel
+      case (EmptyModel, p2) => p2
+      case (p1, EmptyModel) => p1
+      case (p1, p2) => new DrawableProductModel(p1, p2)
+    }
+  }
+
+  override def init(): DrawableModel = this
+
+  override def +(model: Model): Model = throw new UnsupportedOperationException()
+
+  override def hasMaterial(material: Material[_, _]): Boolean =
+    model1.hasMaterial(material) || model2.hasMaterial(material)
+}
