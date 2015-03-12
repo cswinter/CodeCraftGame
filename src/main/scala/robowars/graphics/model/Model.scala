e92353f35806edc6e692e51f91914ca3fc0401d7
Clemens Winter <clemenswinter1+git@gmail.com>
2015-03-12 16:07:38 +0100

    add some documentation on Model classes

--- src/main/scala/robowars/graphics/model/Model.scala
+++ src/main/scala/robowars/graphics/model/Model.scala
@@ -3,30 +3,37 @@ package robowars.graphics.model
 import robowars.graphics.materials.Material
 
 /**
-  * Plan for model hierachy:
-  * Base trait:
-  * - methods draw()
-  * - method init(): Model
-  * - method +(model: Mode): Model
-  * - method project(material: Material) to get only the component belonging to a specific material
-  *
-  * init() needs to be called to obtain a drawable object.
-  *
-  * Several models:
-  * ModelBuilder just stores vertices, material
-  * cannot be drawn, but can be initialised
-  *
-  * This returns an ActiveModel (or something).
-  * This now only stores a handle to the uploaded vertex data and can be drawn but not modified.
-  *
-  * Composite model, which can be used to collect several models with different materials.
-  *
-  * Usage:
-  * - create individual components as ModelBuilder
-  * - sum all ModelBuilder components
-  * - call init() to obtain drawable model
-  */
+ * Overview of model hierarchy:
+ * - Base trait is Model
+ * - Model has two subtraits: ComposableModel and DrawableModel
+ *
+ * ComposableModel:
+ * - has + method for composition (this merges VBOs)
+ * - has init() method to convert to initialised DrawableModel
+ *
+ * DrawableModel:
+ * - has methods for drawing
+ * - allows for product composition with * (no merging of models)
+ *
+ *
+ * init() needs to be called to obtain a drawable object.
+ *
+ * Several models:
+ * ModelBuilder just stores vertices, material
+ * cannot be drawn, but can be initialised
+ *
+ * This returns an ActiveModel (or something).
+ * This now only stores a handle to the uploaded vertex data and can be drawn but not modified.
+ *
+ * Composite model, which can be used to collect several models with different materials.
+ *
+ * Usage:
+ * - create individual components as ModelBuilder
+ * - sum all static ModelBuilder components
+ * - call init() to obtain drawable model
+ */
 
+// TODO: add more doc on * composition, static vs animated, wrapper models
 
 trait Model {
   def project(material: Material[_, _]): Model
