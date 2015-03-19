35c283914846efbbe937e040f78253628ce9d4c5
Clemens Winter <clemenswinter1+git@gmail.com>
2015-03-19 14:53:09 +0100

    add dispose() function to VBO

--- src/main/scala/robowars/graphics/model/VBO.scala
+++ src/main/scala/robowars/graphics/model/VBO.scala
@@ -1,10 +1,20 @@
 package robowars.graphics.model
 
+
+import javax.media.opengl._
+
+
 /**
  * Vertex Buffer Object
  */
 case class VBO(id: Int, size: Int, vao: Int) {
   VBO._count += 1
+
+  def dispose()(implicit gl: GL4): Unit = {
+    gl.glDeleteBuffers(1, Array(id), 0)
+    gl.glDeleteVertexArrays(1, Array(vao), 0)
+    VBO._count -= 1
+  }
 }
 
 
