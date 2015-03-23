bfd45c3df9c45404aa92ad59483b60c42e95a87b
Clemens Winter <clemenswinter1+git@gmail.com>
2015-03-23 18:25:49 +0100

    move Polygon class to separate file

---
+++ src/main/scala/robowars/graphics/model/Polygon.scala
@@ -0,0 +1,42 @@
+package robowars.graphics.model
+
+import robowars.graphics.materials.Material
+
+import scala.reflect.ClassTag
+
+
+case class Polygon[TColor <: Vertex : ClassTag, TParams](
+  material: Material[VertexXYZ, TColor, TParams],
+  n: Int,
+  colorMidpoint: TColor,
+  colorOutside: TColor,
+  radius: Float = 1,
+  position: VertexXY = NullVectorXY,
+  zPos: Float = 0,
+  orientation: Float = 0
+) extends PrimitiveModelBuilder[Polygon[TColor, TParams], TColor, TParams] {
+  val shape = this
+
+  protected override def computeVertexData(): Seq[(VertexXYZ, TColor)] = {
+    val vertices = Geometry.polygonVertices2(n, orientation, radius, position)
+    val vertexPos = new Array[VertexXYZ](3 * n)
+    for (i <- 0 until n) {
+      val v1 = if (i == 0) vertices(n - 1) else vertices(i - 1)
+      val v2 = vertices(i)
+
+      vertexPos(3 * i + 0) = position.zPos(zPos)
+      vertexPos(3 * i + 1) = v1.zPos(zPos)
+      vertexPos(3 * i + 2) = v2.zPos(zPos)
+    }
+
+
+    val colors = new Array[TColor](vertexPos.length)
+    for (i <- 0 until n) {
+      colors(3 * i + 1) = colorOutside
+      colors(3 * i + 2) = colorOutside
+      colors(3 * i) = colorMidpoint
+    }
+
+    vertexPos zip colors
+  }
+}
