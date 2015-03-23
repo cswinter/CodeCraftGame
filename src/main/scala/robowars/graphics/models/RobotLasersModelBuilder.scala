a4e173abaefe555baae6f9fe425b1986a0e6f92f
Clemens Winter <clemenswinter1+git@gmail.com>
2015-03-23 18:24:50 +0100

    simplify robot lasers module

--- src/main/scala/robowars/graphics/models/RobotLasersModelBuilder.scala
+++ src/main/scala/robowars/graphics/models/RobotLasersModelBuilder.scala
@@ -10,25 +10,14 @@ case class RobotLasersModelBuilder(position: VertexXY, n: Int)(implicit rs: Rend
   override def signature: RobotLasersModelBuilder = this
 
   override protected def buildModel: Model[Unit] = {
-
-    val posCharges = Geometry.polygonVertices2(3, radius = 5)
-    val charges =
-      for (pos <- posCharges)
-        yield Polygon(rs.MaterialXYRGB, 15, White, White, 1.5f, pos + position, zPos = 2).getModel
-
-
-    new StaticCompositeModel(
-      Seq(
-        Polygon(
-          rs.MaterialXYRGB,
-          4,
-          ColorRGB(0.65f, 0.65f, 0.75f),
-          ColorRGB(0.65f, 0.65f, 0.75f),
-          radius = 3,
-          position = position,
-          zPos = 1
-        ).getModel
-      ) ++ charges
-    )
+    Polygon(
+      rs.MaterialXYRGB,
+      3,
+      White,
+      ColorThrusters,
+      radius = 8,
+      position = position,
+      zPos = 1
+    ).getModel
   }
 }
