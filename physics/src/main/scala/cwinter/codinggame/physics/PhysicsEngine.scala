0210a5c15ce4fe57d31c61b6e4c56b1b0ee122a0
Clemens Winter <clemenswinter1+git@gmail.com>
2015-04-16 18:08:29 +0200

    add ObjectRecord wrapper in PhyisicsEngine for bookkeeping

--- physics/src/main/scala/cwinter/codinggame/physics/PhysicsEngine.scala
+++ physics/src/main/scala/cwinter/codinggame/physics/PhysicsEngine.scala
@@ -4,16 +4,15 @@ import cwinter.codinggame.maths.Rectangle
 
 
 class PhysicsEngine[T <: DynamicObject[T]](val boundingRectangle: Rectangle) {
-  private[this] val objects = collection.mutable.ArrayBuffer.empty[T]
+  private[this] val objects = collection.mutable.ArrayBuffer.empty[ObjectRecord]
   private[this] var time: Double = 0
   private[this] var nextTime: Double = 0
   private[this] var discreteTime: Int = 0
-  private[this] var collisionTable: Map[T, Collision] = null
-  val events = collection.mutable.PriorityQueue[Collision]()
+  private[this] val events = collection.mutable.PriorityQueue[Collision]()
 
 
   def addObject(obj: T): Unit = {
-    objects += obj
+    objects += new ObjectRecord(obj)
   }
 
   /**
@@ -23,8 +22,7 @@ class PhysicsEngine[T <: DynamicObject[T]](val boundingRectangle: Rectangle) {
     discreteTime += 1
     nextTime = discreteTime / 30.0
 
-    collisionTable = Map.empty[T, Collision]
-    objects.foreach(updateNextCollision)
+    objects.foreach(x => updateNextCollision(x))
 
     while (events.size > 0) {
       val collision = events.dequeue()
@@ -40,7 +38,7 @@ class PhysicsEngine[T <: DynamicObject[T]](val boundingRectangle: Rectangle) {
 
 
       println(f"collision at ts=$discreteTime: $collision")
-      if (collisionTable.contains(collision.obj) && collisionTable(collision.obj) == collision) {
+      if (collision.obj.nextCollision == Some(collision)) {
         if (collision.time > time) {
           collision.involvedObjects.foreach(_.updatePosition(collision.time))
           time = collision.time
@@ -48,37 +46,36 @@ class PhysicsEngine[T <: DynamicObject[T]](val boundingRectangle: Rectangle) {
         collision match {
           case ObjectWallCollision(obj, _) => obj.handleWallCollision(boundingRectangle)
           case ObjectObjectCollision(obj1, obj2, _) =>
-            if (collisionTable.contains(obj1) &&
-              collisionTable.contains(obj2) &&
-              collisionTable(obj1) == collision && // redundant
-              collisionTable(obj2) == collision) {
+            if (obj2.nextCollision == Some(collision)) {
               obj1.handleObjectCollision(obj2)
             }
         }
-        for (obj <- collision.involvedObjects) collisionTable -= obj
+        for (obj <- collision.involvedObjects) obj.nextCollision = None
 
         collision.involvedObjects.foreach(updateNextCollision)
       }
     }
 
-    objects.foreach(_.updatePosition(nextTime))
+    objects.foreach(_.obj.updatePosition(nextTime))
     time = nextTime
   }
 
 
-  private def updateNextCollision(obj: T): Unit = {
+  private def updateNextCollision(obj: ObjectRecord): Unit = {
     val collisions = computeCollisions(obj)
     if (collisions.nonEmpty) {
       val nextCol = collisions.minBy(_.time)
-      collisionTable += obj -> nextCol
+      val nextColOpt = Some(nextCol)
+      obj.nextCollision = nextColOpt
       events.enqueue(nextCol)
       nextCol match {
         case ObjectObjectCollision(_, obj2, t) =>
-          if (!collisionTable.contains(obj2) ||
-            (collisionTable(obj2).isInstanceOf[ObjectObjectCollision] &&
-              collisionTable(obj2).asInstanceOf[ObjectObjectCollision].involvedObjects.contains(obj)) ||
-            collisionTable(obj2).time >= t) {
-            collisionTable += obj2 -> nextCol
+          // TODO: make this not horrible
+          if (obj2.nextCollision == None ||
+            (obj2.nextCollision.get.isInstanceOf[ObjectObjectCollision] &&
+              obj2.nextCollision.get.asInstanceOf[ObjectObjectCollision].involvedObjects.contains(obj)) ||
+            obj2.nextCollision.get.time >= t) {
+            obj2.nextCollision = nextColOpt
           }
         case _ =>
       }
@@ -86,18 +83,29 @@ class PhysicsEngine[T <: DynamicObject[T]](val boundingRectangle: Rectangle) {
   }
 
 
-  private def computeCollisions(obj: T): Iterable[Collision] = {
+  private final class ObjectRecord(
+    val obj: T,
+    var cellX: Int = 0,
+    var cellY: Int = 0,
+    var nextCollision: Option[Collision] = None,
+    var nextTransfer: Any = null
+  )
+
+  private implicit def objectRecordIsT(objRec: ObjectRecord): T = objRec.obj
+
+
+  private def computeCollisions(obj: ObjectRecord): Iterable[Collision] = {
     val nearbyObjects = objects
-    nearbyObjects.foreach(_.updatePosition(time))
+    nearbyObjects.foreach(_.obj.updatePosition(time))
     val objectObjectCollisions =
       for {
         obji <- nearbyObjects
-        dt <- obj.collisionTime(obji, nextTime)
+        dt <- obj.obj.collisionTime(obji.obj, nextTime)
       } yield ObjectObjectCollision(obj, obji, time + dt)
 
     val objectWallCollisions =
       for {
-        dt <- obj.wallCollisionTime(boundingRectangle, nextTime)
+        dt <- obj.obj.wallCollisionTime(boundingRectangle, nextTime)
       } yield ObjectWallCollision(obj, time + dt)
 
     objectObjectCollisions ++ objectWallCollisions
@@ -106,20 +114,27 @@ class PhysicsEngine[T <: DynamicObject[T]](val boundingRectangle: Rectangle) {
 
   private sealed trait Collision extends Ordered[Collision] {
     val time: Double
-    def involves(obj: T): Boolean
-    def involvedObjects: Seq[T]
-    def obj: T
+    def involves(obj: ObjectRecord): Boolean
+    def involvedObjects: Seq[ObjectRecord]
+    def obj: ObjectRecord
     override def compare(that: Collision): Int = that.time compare time
   }
 
-  private final case class ObjectObjectCollision(obj1: T, obj2: T, time: Double) extends Collision {
-    def involves(obj: T): Boolean = obj == obj1 || obj == obj2
-    def obj: T = obj1
-    def involvedObjects: Seq[T] = Seq(obj1, obj2)
+  private final case class ObjectObjectCollision(
+    obj1: ObjectRecord,
+    obj2: ObjectRecord,
+    time: Double
+  ) extends Collision {
+    def involves(obj: ObjectRecord): Boolean = obj == obj1 || obj == obj2
+    def obj: ObjectRecord = obj1
+    def involvedObjects: Seq[ObjectRecord] = Seq(obj1, obj2)
   }
 
-  private final case class ObjectWallCollision(obj: T, time: Double) extends Collision {
-    def involves(obj: T): Boolean = obj == this.obj
-    def involvedObjects: Seq[T] = Seq(obj)
+  private final case class ObjectWallCollision(
+    obj: ObjectRecord,
+    time: Double
+  ) extends Collision {
+    def involves(obj: ObjectRecord): Boolean = obj == this.obj
+    def involvedObjects: Seq[ObjectRecord] = Seq(obj)
   }
 }
