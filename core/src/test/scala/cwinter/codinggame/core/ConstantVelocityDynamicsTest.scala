3577e667bc09d11606eba452fac7fa01c62967cd
Clemens Winter <clemenswinter1@gmail.com>
2015-05-05 17:56:48 +0200

    add tests for ConstantVelocityDynamics

---
+++ core/src/test/scala/cwinter/codinggame/core/ConstantVelocityDynamicsTest.scala
@@ -0,0 +1,19 @@
+package cwinter.codinggame.core
+
+import cwinter.codinggame.util.maths.{Rectangle, Vector2}
+import org.scalatest.FlatSpec
+
+class ConstantVelocityDynamicsTest extends FlatSpec {
+  object ConstVelTestDynamics extends ConstantVelocityDynamics(1, Vector2.NullVector, 0) {
+    override def update(): Unit = ()
+    override def handleWallCollision(areaBounds: Rectangle): Unit = ()
+    override def handleObjectCollision(other: ConstantVelocityDynamics): Unit = ()
+    def setVelocity(v: Vector2): Unit = velocity = v
+  }
+
+  "ConstantVelocityDynamics" should "move with linear velocity" in {
+    ConstVelTestDynamics.setVelocity(Vector2(1, 1))
+    ConstVelTestDynamics.updatePosition(10)
+    assert(ConstVelTestDynamics.pos == Vector2(10, 10))
+  }
+}
