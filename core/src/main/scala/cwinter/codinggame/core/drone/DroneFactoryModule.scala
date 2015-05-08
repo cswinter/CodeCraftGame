038ef3186a6c806f31c7939a3526cd6cfa2524ae
Clemens Winter <clemenswinter1@gmail.com>
2015-05-08 13:56:52 +0200

    remove dead code (factory drone construction)

--- core/src/main/scala/cwinter/codinggame/core/drone/DroneFactoryModule.scala
+++ core/src/main/scala/cwinter/codinggame/core/drone/DroneFactoryModule.scala
@@ -11,9 +11,7 @@ class DroneFactoryModule(positions: Seq[Int], owner: Drone)
   final val MineralResourceYield = 7
 
 
-  private[this] var newDrones = List.empty[Drone]
   private[this] var newMinerals = List.empty[MineralCrystal]
-  private[this] var droneConstructions = List.empty[(Drone, Int)]
   private[this] var mineralProcessing = List.empty[(MineralCrystal, Int)]
 
 
@@ -21,65 +19,20 @@ class DroneFactoryModule(positions: Seq[Int], owner: Drone)
     var effects = List.empty[SimulatorEvent]
     var remainingResources = availableResources
 
-    // start new drone constructions
-    for (drone <- newDrones) {
-      droneConstructions ::= ((drone, 0))
-      droneConstructions = droneConstructions.sortBy { case (c, p) => -drone.requiredFactories }
-      drone.dynamics.orientation = owner.dynamics.orientation
-      effects ::= DroneConstructionStarted(drone)
-    }
-    newDrones = List.empty[Drone]
-
     // start new mineral constructions
     for (mineral <- newMinerals) {
       mineralProcessing ::= ((mineral, mineral.size * MineralResourceYield * MineralProcessingPeriod))
-      mineralProcessing = mineralProcessing.sortBy { case (m, p) => -m.size }
       effects ::= MineralCrystalActivated(mineral)
     }
     newMinerals = List.empty[MineralCrystal]
 
 
     // determine factory positions
-    val partitioning = droneConstructions.map(_._1.requiredFactories) ++ mineralProcessing.map(_._1.size)
-    val centers = absoluteMergedModulePositions(partitioning)
-
-    // perform drone constructions
-    droneConstructions =
-      for (((drone, progress), center) <- droneConstructions zip centers.take(droneConstructions.size))
-        yield {
-          drone.dynamics.setPosition(center)
-          drone.dynamics.orientation = owner.dynamics.orientation
-          drone.constructionProgress = Some(progress)
-
-          val progress2 =
-          if (progress % drone.resourceDepletionPeriod == 0) {
-            if (remainingResources > 0) {
-              remainingResources -= 1
-              progress + 1
-            } else {
-              progress
-            }
-          } else {
-            progress + 1
-          }
-
-          if (progress2 == drone.buildTime) {
-            effects ::= SpawnDrone(drone)
-            drone.constructionProgress = None
-            drone.dynamics.setPosition(owner.position - 150 * Rng.vector2())
-          }
-
-          (drone, progress2)
-        }
-
-    droneConstructions = droneConstructions.filter {
-      case (drone, progress) =>
-        progress < drone.buildTime
-    }
+    val centers = absoluteMergedModulePositions(contents)
 
     // perform mineral processing
     mineralProcessing =
-      for (((mineral, progress), center) <- mineralProcessing zip centers.drop(droneConstructions.size))
+      for (((mineral, progress), center) <- mineralProcessing zip centers)
         yield {
           mineral.position = center
 
@@ -100,11 +53,6 @@ class DroneFactoryModule(positions: Seq[Int], owner: Drone)
   }
 
 
-  def startDroneConstruction(command: ConstructDrone): Unit = {
-    newDrones ::= command.drone
-  }
-
-
   def startMineralProcessing(mineral: MineralCrystal): Unit = {
     // TODO: need a capacity check somewhere
     newMinerals ::= mineral
@@ -112,20 +60,15 @@ class DroneFactoryModule(positions: Seq[Int], owner: Drone)
 
 
   def currentCapacity: Int = positions.length -
-    droneConstructions.foldLeft(0)(_ + _._1.requiredFactories) -
     mineralProcessing.foldLeft(0)(_ + _._1.size) -
-    newMinerals.foldLeft(0)(_ + _.size) -
-    newDrones.foldLeft(0)(_ + _.requiredFactories)
+    newMinerals.foldLeft(0)(_ + _.size)
 
-  def contents: Seq[Int] = (
-    droneConstructions.map(_._1.requiredFactories) ++
-      mineralProcessing.map(_._1.size)
-  ).sorted.reverse
+  private def contents: Seq[Int] = mineralProcessing.map(_._1.size).sorted.reverse
 
   override def descriptors: Seq[worldstate.DroneModule] = {
-    val partitioning = mineralProcessing.map(_._1.size)
+    val partitioning = contents
     val processingMinerals = partitionIndices(partitioning)
-    val idle = positions.drop(mineralProcessing.foldLeft(0)(_ + _._1.size)).map(Seq(_))
+    val idle = positions.drop(contents.sum).map(Seq(_))
     (processingMinerals ++ idle).map(worldstate.ProcessingModule(_))
   }
 }
