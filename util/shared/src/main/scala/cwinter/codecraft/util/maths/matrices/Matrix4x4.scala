d385735613dee98cd92fdd0d7ba041f0ac58b255
Clemens Winter <codecraftgame@gmail.com>
2015-07-01 22:42:56 +0200

    perf: manually unrolled 4x4 matrix multiplication
    seems to be significantly faster.
    running in chrome, framerate increase from ~17 to ~25.
    matrix multiplication accounted for 30% of runtime before, less than 4% afterwards!
    presumably most of the savings come from eliminating triple for comprehension and sum function.
    so just using while loops might work as well.

--- util/shared/src/main/scala/cwinter/codecraft/util/maths/matrices/Matrix4x4.scala
+++ util/shared/src/main/scala/cwinter/codecraft/util/maths/matrices/Matrix4x4.scala
@@ -6,11 +6,30 @@ class Matrix4x4(val data: Array[Float]) {
   assert(data.length == 16)
 
   def *(right: Matrix4x4): Matrix4x4 = {
+    // code generated with the following expression:
+    // (for (col <- 0 to 3; row <- 0 to 3) yield { s"result($row, $col) = " + (for (i <- 0 to 3) yield s"left($row, $i) * right($i, $col)").mkString(" + ") }).mkString("\n")
+    // to prevent truncation of output by scala repl:
+    // scala> :power
+    // scala> vals.issettings.maxPrintString = 1000
+    // TODO: benchmark. is this actually faster than while loop?
+    // TODO: also, add mutating variant to eliminate array allocation
     val result = new Matrix4x4(new Array[Float](16))
-    for (col <- 0 to 3; row <- 0 to 3)
-      result(row, col) =
-        (for (i <- 0 to 3) yield left(row, i) * right(i, col)).sum
-
+    result(0, 0) = left(0, 0) * right(0, 0) + left(0, 1) * right(1, 0) + left(0, 2) * right(2, 0) + left(0, 3) * right(3, 0)
+    result(1, 0) = left(1, 0) * right(0, 0) + left(1, 1) * right(1, 0) + left(1, 2) * right(2, 0) + left(1, 3) * right(3, 0)
+    result(2, 0) = left(2, 0) * right(0, 0) + left(2, 1) * right(1, 0) + left(2, 2) * right(2, 0) + left(2, 3) * right(3, 0)
+    result(3, 0) = left(3, 0) * right(0, 0) + left(3, 1) * right(1, 0) + left(3, 2) * right(2, 0) + left(3, 3) * right(3, 0)
+    result(0, 1) = left(0, 0) * right(0, 1) + left(0, 1) * right(1, 1) + left(0, 2) * right(2, 1) + left(0, 3) * right(3, 1)
+    result(1, 1) = left(1, 0) * right(0, 1) + left(1, 1) * right(1, 1) + left(1, 2) * right(2, 1) + left(1, 3) * right(3, 1)
+    result(2, 1) = left(2, 0) * right(0, 1) + left(2, 1) * right(1, 1) + left(2, 2) * right(2, 1) + left(2, 3) * right(3, 1)
+    result(3, 1) = left(3, 0) * right(0, 1) + left(3, 1) * right(1, 1) + left(3, 2) * right(2, 1) + left(3, 3) * right(3, 1)
+    result(0, 2) = left(0, 0) * right(0, 2) + left(0, 1) * right(1, 2) + left(0, 2) * right(2, 2) + left(0, 3) * right(3, 2)
+    result(1, 2) = left(1, 0) * right(0, 2) + left(1, 1) * right(1, 2) + left(1, 2) * right(2, 2) + left(1, 3) * right(3, 2)
+    result(2, 2) = left(2, 0) * right(0, 2) + left(2, 1) * right(1, 2) + left(2, 2) * right(2, 2) + left(2, 3) * right(3, 2)
+    result(3, 2) = left(3, 0) * right(0, 2) + left(3, 1) * right(1, 2) + left(3, 2) * right(2, 2) + left(3, 3) * right(3, 2)
+    result(0, 3) = left(0, 0) * right(0, 3) + left(0, 1) * right(1, 3) + left(0, 2) * right(2, 3) + left(0, 3) * right(3, 3)
+    result(1, 3) = left(1, 0) * right(0, 3) + left(1, 1) * right(1, 3) + left(1, 2) * right(2, 3) + left(1, 3) * right(3, 3)
+    result(2, 3) = left(2, 0) * right(0, 3) + left(2, 1) * right(1, 3) + left(2, 2) * right(2, 3) + left(2, 3) * right(3, 3)
+    result(3, 3) = left(3, 0) * right(0, 3) + left(3, 1) * right(1, 3) + left(3, 2) * right(2, 3) + left(3, 3) * right(3, 3)
     result
   }
 
