3b0858074aca66919bd2b8af3e8842e01f764815
Clemens Winter <codecraftgame@gmail.com>
2015-07-02 12:15:35 +0200

    fix: missing ; in rgba_gaussian_fs.glsl

--- graphics/jvm/src/main/resources/rgba_gaussian_fs.glsl
+++ graphics/jvm/src/main/resources/rgba_gaussian_fs.glsl
@@ -6,7 +6,7 @@ out vec4 outputCol;
 
 
 void main() {
-    float x = 1 - fragmentCol.w
+    float x = 1 - fragmentCol.w;
     float alpha = exp(-5 * x * x);
     outputCol = vec4(fragmentCol.x, fragmentCol.y, fragmentCol.z, alpha);
 }
