f5e31d1376f414072a8e832c259b25ad065b71cf
Clemens Winter <codecraftgame@gmail.com>
2015-09-14 14:41:50 +0200

    feat: 0.1.7.2

--- project/commons.scala
+++ project/commons.scala
@@ -2,7 +2,7 @@ import sbt._
 import Keys._
 
 object Commons {
-  val appVersion = "0.1.7.1"
+  val appVersion = "0.1.7.2"
 
   val settings: Seq[Def.Setting[_]] = Seq(
     organization := "org.cwinter",
