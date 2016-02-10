35413588ac919940408f19139e770f50c5545a8b
Clemens Winter <clemenswinter1@gmail.com>
2016-02-10 11:44:55 +0100

    Delete old Replicator code in testai project

--- testai/src/main/scala/cwinter/codecraft/testai/replicator/SearchToken.scala
+++
@@ -1,9 +0,0 @@
-package cwinter.codecraft.testai.replicator
-
-import cwinter.codecraft.core.api.DroneSpec
-import cwinter.codecraft.util.maths.Vector2
-
-
-case class SearchToken(x: Int, y: Int) {
-  val pos: Vector2 = Vector2((x + 0.5) * DroneSpec.SightRadius, (y + 0.5) * DroneSpec.SightRadius)
-}
