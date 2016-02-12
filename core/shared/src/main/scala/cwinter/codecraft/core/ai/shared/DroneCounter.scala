a17f2ace424305c218082e8ea302a60bc3ee7542
Clemens Winter <clemenswinter1@gmail.com>
2016-02-12 15:37:22 +0100

    Fix DroneCounter

--- core/shared/src/main/scala/cwinter/codecraft/core/ai/shared/DroneCounter.scala
+++ core/shared/src/main/scala/cwinter/codecraft/core/ai/shared/DroneCounter.scala
@@ -3,12 +3,12 @@ package cwinter.codecraft.core.ai.shared
 class DroneCounter {
   private[this] var counts = Map.empty[Class[_], Int]
 
-  def apply[T](clazz: Class[T]): Int = 0
+  def apply[T](clazz: Class[T]): Int = counts.getOrElse(clazz, 0)
 
   def increment[T](clazz: Class[T]): Unit =
-    counts = counts.updated(clazz, counts.getOrElse(clazz, 0) + 1)
+    counts = counts.updated(clazz, this(clazz) + 1)
 
   def decrement[T](clazz: Class[T]): Unit =
-    counts = counts.updated(clazz, counts.getOrElse(clazz, 0) - 1)
+    counts = counts.updated(clazz, this(clazz) - 1)
 }
 
