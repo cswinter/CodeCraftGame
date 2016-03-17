efa72878591d3fd0562fa97138099643233a6c03
Clemens Winter <clemenswinter1@gmail.com>
2016-03-17 17:25:09 +0100

    Small bugfix

--- graphics/shared/src/main/scala/cwinter/codecraft/graphics/worldstate/WorldObjectDescriptor.scala
+++ graphics/shared/src/main/scala/cwinter/codecraft/graphics/worldstate/WorldObjectDescriptor.scala
@@ -124,7 +124,7 @@ private[codecraft] object PlainEnergyGlobeDescriptor extends EnergyGlobeDescript
 private[codecraft] case class MineralDescriptor(size: Int, xPos: Float, yPos: Float, orientation: Float)
     extends WorldObjectDescriptor[Unit] {
   override def intersects(xPos: Float, yPos: Float, rectangle: Rectangle): Boolean =
-    intersects(xPos, yPos, rectangle, 0) // FIXME
+    intersects(this.xPos, this.yPos, rectangle, 50)
 }
 
 
