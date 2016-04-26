ec073d191d41759550b79b3a290bddf79c9eef00
Clemens Winter <clemenswinter1@gmail.com>
2016-04-26 15:53:18 +0200

    Fix bug breaking OpenGL 2 fallback

--- graphics/jvm/src/main/scala/cwinter/codecraft/graphics/model/JVMVBO.scala
+++ graphics/jvm/src/main/scala/cwinter/codecraft/graphics/model/JVMVBO.scala
@@ -2,19 +2,24 @@ package cwinter.codecraft.graphics.model
 
 import javax.media.opengl._
 
-
 /**
- * Vertex Buffer Object
- */
+  * Vertex Buffer Object
+  */
 private[graphics] case class JVMVBO(id: Int, size: Int, vao: Int) extends VBO {
   def withSize(size: Int): JVMVBO = copy(size = size)
   override def dispose(anyGL: Any): Unit = {
     super.dispose(anyGL)
-    assert(anyGL.isInstanceOf[GL4], s"Expected gl of type javax.media.opengl.GL4. Actual: ${anyGL.getClass.getName}")
-    val gl = anyGL.asInstanceOf[GL4]
-    gl.glDeleteBuffers(1, Array(id), 0)
-    gl.glDeleteVertexArrays(1, Array(vao), 0)
+    anyGL match {
+      case gl2: GL2 =>
+        gl2.glDeleteBuffers(1, Array(id), 0)
+        gl2.glDeleteVertexArrays(1, Array(vao), 0)
+      case gl4: GL4 =>
+        gl4.glDeleteBuffers(1, Array(id), 0)
+        gl4.glDeleteVertexArrays(1, Array(vao), 0)
+      case _ =>
+        throw new Exception(
+            s"Expected gl of type javax.media.opengl.GL2 or javax.media.opengl.GL4. Actual: ${anyGL.getClass.getName}")
+    }
     VBO._count -= 1
   }
 }
-
