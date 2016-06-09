3f4a02530eaf54b6658ffe5beb93ec42a41dd602
Clemens Winter <clemenswinter1@gmail.com>
2016-06-09 12:29:29 +0200

    Measure message sizes

--- core/jvm/src/main/scala/cwinter/codecraft/core/multiplayer/RemoteWebsocketClient.scala
+++ core/jvm/src/main/scala/cwinter/codecraft/core/multiplayer/RemoteWebsocketClient.scala
@@ -5,6 +5,7 @@ import java.nio.ByteBuffer
 import cwinter.codecraft.core.api.{OrangePlayer, BluePlayer, Player}
 import cwinter.codecraft.core.objects.drone._
 import cwinter.codecraft.core.{SimulationContext, WorldMap}
+import cwinter.codecraft.util.AggregateStatistics
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Future, Promise}
@@ -13,9 +14,13 @@ import scala.concurrent.{Future, Promise}
 private[core] class RemoteWebsocketClient(
   override val players: Set[Player],
   val map: WorldMap,
-  val debug: Boolean = false
+  val debug: Boolean = false,
+  val info: Boolean = true
 ) extends RemoteClient with WebsocketWorker {
-  private[this] var clientCommands = Promise[Seq[(Int, SerializableDroneCommand)]]
+  private var clientCommands = Promise[Seq[(Int, SerializableDroneCommand)]]
+  private val commandsSentSize = new AggregateStatistics
+  private val commandsReceivedSize = new AggregateStatistics
+  private val positionsSentSize = new AggregateStatistics
 
 
   override def receive(message: String): Unit = {
@@ -31,6 +36,7 @@ private[core] class RemoteWebsocketClient(
 
   override def receiveBytes(message: ByteBuffer): Unit = {
     try {
+      commandsReceivedSize.addMeasurement(message.remaining())
       handleMessage(MultiplayerMessage.parseBytes(message))
     } catch {
       case t: Throwable =>
@@ -45,9 +51,10 @@ private[core] class RemoteWebsocketClient(
     case WorldStateMessage(_) => throw new Exception("Authoritative server received WorldStateMessage!")
     case _: InitialSync => throw new Exception("Authoritative server received InitialSync!")
     case Register => send(syncMessage)
-    case RTT(time, message) =>
+    case RTT(time, message) => if (debug) {
       val ms = (System.nanoTime - time) / 1000000.0
       println(f"RTT for $message: $ms%.2fms")
+    }
   }
 
   def syncMessage =
@@ -62,24 +69,32 @@ private[core] class RemoteWebsocketClient(
   override def waitForCommands()(implicit context: SimulationContext): Future[Seq[(Int, DroneCommand)]] = {
     if (debug) println(s"[t=${context.timestep}] Waiting for commands...")
     for (commands <- clientCommands.future) yield {
-      if (debug)
-        println("Commands received.")
+      if (debug) println("Commands received.")
       clientCommands = Promise[Seq[(Int, SerializableDroneCommand)]]
       deserialize(commands)
     }
   }
 
   override def sendWorldState(worldState: Iterable[DroneStateMessage]): Unit = {
-    send(MultiplayerMessage.serializeBinary(RTT(System.nanoTime, "ws")))
-    send(MultiplayerMessage.serializeBinary(worldState))
+    val serialized = MultiplayerMessage.serializeBinary(worldState)
+    positionsSentSize.addMeasurement(serialized.remaining)
+    send(serialized)
+    if (info && positionsSentSize.count > 0 && positionsSentSize.count % 1000 == 0) {
+      println(s"Positions Message Size: ${positionsSentSize.display}")
+      println(s"Commands Sent Message Size: ${commandsSentSize.display}")
+      println(s"Commands Received Message Size: ${commandsReceivedSize.display}")
+      val bandwidthEstimate =
+        (commandsReceivedSize.ema + commandsSentSize.ema + positionsSentSize.ema) * 8 * 60 / 1000
+      println(f"Required bandwidth for 60FPS: $bandwidthEstimate%.2gKbps")
+    }
   }
 
   def sendCommands(commands: Seq[(Int, DroneCommand)]): Unit = {
-    send(MultiplayerMessage.serializeBinary(RTT(System.nanoTime, "sc")))
     val serializable =
       for ((id, command) <- commands)
         yield (id, command.toSerializable)
     val serialized = MultiplayerMessage.serializeBinary(serializable)
+    commandsSentSize.addMeasurement(serialized.remaining)
     send(serialized)
   }
 
