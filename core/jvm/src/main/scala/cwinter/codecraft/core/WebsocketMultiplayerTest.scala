ad080ac3c301a39f96156ba7ed9591379fa01a1e
Clemens Winter <clemenswinter1@gmail.com>
2016-06-10 15:46:21 +0200

    Remove dead code

--- core/jvm/src/main/scala/cwinter/codecraft/core/WebsocketMultiplayerTest.scala
+++ core/jvm/src/main/scala/cwinter/codecraft/core/WebsocketMultiplayerTest.scala
@@ -8,22 +8,6 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 private[core] object WebsocketMultiplayerTest {
   def main(args: Array[String]): Unit = {
-    /*
-    import boopickle.Default._
-    /*implicit val mpMsgPickler = compositePickler[MultiplayerMessage]
-        .addConcreteType[CommandsMessage]
-        .addConcreteType[InitialSync]
-        .addConcreteType[Register.type]*/
-
-    val msg = MultiplayerMessage.serializeBinary(Seq((0, SerializableMoveToMineralCrystal(0))))
-    println(msg)
-    val decoded = MultiplayerMessage.parseBytes(msg)
-    println(decoded)
-    //val msg = MultiplayerMessage.serializeBinary()
-    //val decoded = MultiplayerMessage.parseBytes(msg)
-
-    return*/
-
     new Thread {
       override def run(): Unit = {
         multiplayer.Server.spawnServerInstance()
