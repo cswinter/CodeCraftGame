41e928258d78e8420b6fc902eb101ccacf630876
Clemens Winter <clemenswinter1@gmail.com>
2016-06-20 16:06:01 +0200

    Fix MultiplayerTest

--- core/jvm/src/main/scala/cwinter/codecraft/core/MultiplayerTest.scala
+++ core/jvm/src/main/scala/cwinter/codecraft/core/MultiplayerTest.scala
@@ -2,13 +2,13 @@ package cwinter.codecraft.core
 
 import cwinter.codecraft.core.api.TheGameMaster
 
-import scala.async.Async.{async, await}
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 
 private[core] object MultiplayerTest {
-  def main(args: Array[String]): Unit = async {
-    val client = await { TheGameMaster.prepareMultiplayerGame("192.168.2.113", TheGameMaster.level2AI()) }
+  def main(args: Array[String]): Unit = {
+    val client = Await.result(TheGameMaster.prepareMultiplayerGame("192.168.2.107", TheGameMaster.replicatorAI()), 10.seconds)
     TheGameMaster.run(client)
   }
 }
