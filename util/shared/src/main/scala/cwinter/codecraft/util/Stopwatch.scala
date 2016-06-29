702ccf90ee17872ca2a7bf80a3d05d9eb752cf4a
Clemens Winter <clemenswinter1@gmail.com>
2016-06-29 14:37:27 +0200

    Add percentage metric to performance monitor

--- util/shared/src/main/scala/cwinter/codecraft/util/Stopwatch.scala
+++ util/shared/src/main/scala/cwinter/codecraft/util/Stopwatch.scala
@@ -47,29 +47,31 @@ class Stopwatch {
   }
 
   def compileReport: String = {
-    val header = List("Section", "Count", "Mean", "Min", "Max")
+    val header = List("Section", "Percent", "Mean/ms", "Min/ms", "Max/ms", "Count")
+    val totalTime = measurements.values.map(_.sum).sum
     val rows =
       for {
         (name, data) <- measurements.toList.sortBy(x => measurementOrder(x._1))
-        statistics = computeStatistics(data)
+        statistics = computeStatistics(data, totalTime)
       } yield name.toString.tail :: toRow(statistics)
     asciiTable(header :: rows.toList)
   }
 
-  private def computeStatistics(measurements: Seq[Long]) = Statistics(
+  private def computeStatistics(measurements: Seq[Long], total: Long) = Statistics(
     n = measurements.size,
+    percentage = 100.0 * measurements.sum / total,
     mean = toMillis(measurements.sum) / measurements.size,
     min = toMillis(measurements.min),
     max = toMillis(measurements.max)
   )
 
   private def toRow(data: Statistics) = List(
-    f"${data.n}", f"${data.mean}%.3f", f"${data.min}%.3f", f"${data.max}%.3f"
+    f"${data.percentage}%5.1f", f"${data.mean}%.3f", f"${data.min}%.3f", f"${data.max}%.3f", s"${data.n}"
   )
 
   private def toMillis(nanos: Long): Millis = nanos * 0.000001
 
-  private case class Statistics(n: Int, mean: Millis, min: Millis, max: Millis)
+  private case class Statistics(n: Int, percentage: Double, mean: Millis, min: Millis, max: Millis)
 
   private def asciiTable(rows: List[Seq[String]]): String = {
     require(rows.map(_.length).toSet.size == 1, "All rows must have the same length.")
