c17bd17aa0994c2bba6316075daad70aac487d36
Clemens Winter <clemenswinter1@gmail.com>
2016-07-22 08:14:21 +0200

    Tweak formula for generating unique drone ids

--- core/shared/src/main/scala/cwinter/codecraft/core/objects/IDGenerator.scala
+++ core/shared/src/main/scala/cwinter/codecraft/core/objects/IDGenerator.scala
@@ -2,12 +2,17 @@ package cwinter.codecraft.core.objects
 
 
 private[codecraft] class IDGenerator(group: Int) {
+  import IDGenerator._
   private[this] var count: Int = -1
-  // Integer.reverse would be better, this is an easy hack which is JavaScript compatible
-  private[this] val prefix = group * 0x01000000
+  assert(group <= MaxFactions)
 
   def getAndIncrement(): Int = {
     count += 1
-    count + prefix
+    count * MaxFactions + group
   }
 }
+
+private[codecraft] object IDGenerator {
+  final val MaxFactions = 4
+}
+
