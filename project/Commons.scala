1daca72ccc12a4eb94bd46ce974bdce7a101df5f
Clemens Winter <clemenswinter1@gmail.com>
2016-08-29 09:02:56 +0200

    Increment version to 0.5.1

--- project/Commons.scala
+++ project/Commons.scala
@@ -2,7 +2,7 @@ import sbt._
 import Keys._
 
 object Commons {
-  val appVersion = "0.5.1-SNAPSHOT"
+  val appVersion = "0.5.1"
 
   val settings: Seq[Def.Setting[_]] = Seq(
     organization := "org.codecraftgame",
