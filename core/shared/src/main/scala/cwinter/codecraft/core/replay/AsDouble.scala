417865cf3e73d04b82b95cc0e88cc527d2d95472
barrybecker4 <barrybecker4@yahoo.com>
2017-10-22 19:20:49 -0700

    #7 Remove some dead code.

--- core/shared/src/main/scala/cwinter/codecraft/core/replay/AsDouble.scala
+++
@@ -1,9 +0,0 @@
-package cwinter.codecraft.core.replay
-
-private[codecraft] object AsDouble {
-  def unapply(s: String) = try {
-    Some(s.toDouble)
-  } catch {
-    case e: NumberFormatException => None
-  }
-}
