2e91a6e1b3db23b50d8f7fc9e6e76e07c37bc872
Clemens Winter <clemenswinter1@gmail.com>
2020-10-12 12:09:23 -0700

    Tolerate desync harder

--- core/shared/src/main/scala/cwinter/codecraft/core/game/SimulationContext.scala
+++ core/shared/src/main/scala/cwinter/codecraft/core/game/SimulationContext.scala
@@ -3,12 +3,19 @@ package cwinter.codecraft.core.game
 import cwinter.codecraft.core.objects.MineralCrystalImpl
 import cwinter.codecraft.core.objects.drone.DroneImpl
 
-
 private[codecraft] case class SimulationContext(
   droneRegistry: Map[Int, DroneImpl],
   mineralRegistry: Map[Int, MineralCrystalImpl],
   timestep: Int
 ) {
   def drone(id: Int): DroneImpl = droneRegistry(id)
+  def maybeDrone(id: Int): Option[DroneImpl] = {
+    if (droneRegistry.contains(id)) {
+      Some(droneRegistry(id))
+    } else {
+      println(s"WARNING: possible desync, drone with id $id not found")
+      None
+    }
+  }
   def mineral(id: Int): MineralCrystalImpl = mineralRegistry(id)
 }
